7cb3e0315e5fb17af4ed40ab1024f74285489340
Piyush Basak <piyush.basak2014.pb@gmail.com>
2017-12-04 00:23:53 +0530

    Update Hello HankerRank.rb

--- Hello HankerRank.rb
+++ Hello HankerRank.rb
@@ -1,3 +1,10 @@
 <!--Prints Hacker Rank -->
 print "Hello HackerRank!!"
 
+<!--Ruby Tutorial - Everything is an Object, Prints Main -->
+print self
+
+<!-- Ruby Tutorial - Object Methods  -->
+return number.even?
+<or>
+number.even?
